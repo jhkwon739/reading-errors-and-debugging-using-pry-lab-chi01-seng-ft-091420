1c82c4f1fe670e656199dcb7dcb07451c008e102
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-11 03:15:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,12 +7,13 @@ end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
-  binding.pry
 end
 
 def engage
-  date = generate_star_date
   puts state_log(date)
+  date = generate_star_date
+
+  binding.pry
 end
 
 puts engage
